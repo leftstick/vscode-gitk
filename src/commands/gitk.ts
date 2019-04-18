0716048e7bb54a0bbb1cc3e8d070f515303b0347
Howard.Zuo <leftstick@qq.com>
2019-04-18 14:33:09 +0800

    fix: configration listener was registered multiple times

--- src/commands/gitk.ts
+++ src/commands/gitk.ts
@@ -3,14 +3,23 @@ import * as vscode from 'vscode'
 import { GitkContentProvider } from '../gitkViewProvider'
 
 export function registryGitk(context: vscode.ExtensionContext): vscode.Disposable {
-  let provider: GitkContentProvider
+  let provider: GitkContentProvider = new GitkContentProvider()
   let panel: vscode.WebviewPanel
 
-  const gitk = vscode.commands.registerCommand('extension.gitk', async (fileUri?: vscode.Uri) => {
-    if (!provider) {
-      provider = new GitkContentProvider()
-    }
+  let config = vscode.workspace.getConfiguration('gitk')
 
+  vscode.workspace.onDidChangeConfiguration(
+    async () => {
+      config = vscode.workspace.getConfiguration('gitk')
+      provider.setConfig(config)
+      const html = await provider.getInitHtml()
+      panel.webview.html = html
+    },
+    this,
+    context.subscriptions
+  )
+
+  const gitk = vscode.commands.registerCommand('extension.gitk', async (fileUri?: vscode.Uri) => {
     if (
       (!fileUri || !fileUri.fsPath) &&
       (!vscode.window.activeTextEditor || !vscode.window.activeTextEditor.document)
@@ -23,50 +32,36 @@ export function registryGitk(context: vscode.ExtensionContext): vscode.Disposabl
       (fileUri && fileUri.fsPath) ||
       (vscode.window.activeTextEditor && vscode.window.activeTextEditor.document.fileName)
 
-    let config = vscode.workspace.getConfiguration('gitk')
-
-    vscode.workspace.onDidChangeConfiguration(
-      async () => {
-        config = vscode.workspace.getConfiguration('gitk')
-        provider.setConfig(config)
-        const html = await provider.getInitHtml()
-        panel.webview.html = html
-      },
-      this,
-      context.subscriptions
-    )
-
     if (!panel) {
-      panel = vscode.window.createWebviewPanel('gitk', 'gitk', vscode.ViewColumn.One, {
+      panel = vscode.window.createWebviewPanel('gitk', 'Gitk', vscode.ViewColumn.One, {
         enableScripts: true
       })
+      // Reset when the current panel is closed
+      panel.onDidDispose(
+        () => {
+          panel = undefined
+        },
+        null,
+        context.subscriptions
+      )
+
+      // Handle messages from the webview
+      panel.webview.onDidReceiveMessage(
+        async message => {
+          if (message.command === 'read-detail') {
+            const detail = await provider.getDetail(message.payload.hash)
+            panel.webview.postMessage({ command: 'see-detail', payload: detail })
+          }
+          if (message.command === 'hash-copied') {
+            vscode.window.setStatusBarMessage(`hash [${message.payload.hash}] was copied`, 3000)
+          }
+        },
+        undefined,
+        context.subscriptions
+      )
     } else if (!panel.visible) {
       panel.reveal()
     }
-
-    // Reset when the current panel is closed
-    panel.onDidDispose(
-      () => {
-        panel = undefined
-      },
-      null,
-      context.subscriptions
-    )
-
-    // Handle messages from the webview
-    panel.webview.onDidReceiveMessage(
-      async message => {
-        if (message.command === 'read-detail') {
-          const detail = await provider.getDetail(message.payload.hash)
-          panel.webview.postMessage({ command: 'see-detail', payload: detail })
-        }
-        if (message.command === 'hash-copied') {
-          vscode.window.setStatusBarMessage(`hash [${message.payload.hash}] was copied`, 3000)
-        }
-      },
-      undefined,
-      context.subscriptions
-    )
 
     try {
       provider.setConfig(config)
--- src/commands/gitkrepo.ts
+++ src/commands/gitkrepo.ts
@@ -3,63 +3,59 @@ import * as vscode from 'vscode'
 import { gitkRepoViewProvider } from '../gitkRepoViewProvider'
 
 export function registryGitkrepo(context: vscode.ExtensionContext): vscode.Disposable {
-  let provider: gitkRepoViewProvider
+  let provider: gitkRepoViewProvider = new gitkRepoViewProvider()
   let panel: vscode.WebviewPanel
 
+  let config = vscode.workspace.getConfiguration('gitk')
+
+  vscode.workspace.onDidChangeConfiguration(
+    async () => {
+      config = vscode.workspace.getConfiguration('gitk')
+      provider.setConfig(config)
+      const html = await provider.getInitHtml()
+      panel.webview.html = html
+    },
+    this,
+    context.subscriptions
+  )
+
   const gitkrepo = vscode.commands.registerCommand('extension.gitkrepo', async () => {
-    if (!provider) {
-      provider = new gitkRepoViewProvider()
-    }
-
-    let config = vscode.workspace.getConfiguration('gitk')
-
-    vscode.workspace.onDidChangeConfiguration(
-      async () => {
-        config = vscode.workspace.getConfiguration('gitk')
-        provider.setConfig(config)
-        const html = await provider.getInitHtml()
-        panel.webview.html = html
-      },
-      this,
-      context.subscriptions
-    )
-
     if (!panel) {
-      panel = vscode.window.createWebviewPanel('gitk', 'gitk', vscode.ViewColumn.One, {
+      panel = vscode.window.createWebviewPanel('gitk', 'Gitk Repo', vscode.ViewColumn.One, {
         enableScripts: true
       })
+
+      // Reset when the current panel is closed
+      panel.onDidDispose(
+        () => {
+          panel = undefined
+        },
+        null,
+        context.subscriptions
+      )
+
+      // Handle messages from the webview
+      panel.webview.onDidReceiveMessage(
+        async message => {
+          if (message.command === 'read-detail') {
+            const detail = await provider.getDetail(message.payload.hash)
+            panel.webview.postMessage({ command: 'see-detail', payload: detail })
+          }
+          if (message.command === 'hash-copied') {
+            vscode.window.setStatusBarMessage(`hash [${message.payload.hash}] was copied`, 3000)
+          }
+          if (['go-prev', 'go-next'].includes(message.command)) {
+            provider.setPageNum(message.payload.pageNum)
+            const html = await provider.getInitHtml()
+            panel.webview.html = html
+          }
+        },
+        undefined,
+        context.subscriptions
+      )
     } else if (!panel.visible) {
       panel.reveal()
     }
-
-    // Reset when the current panel is closed
-    panel.onDidDispose(
-      () => {
-        panel = undefined
-      },
-      null,
-      context.subscriptions
-    )
-
-    // Handle messages from the webview
-    panel.webview.onDidReceiveMessage(
-      async message => {
-        if (message.command === 'read-detail') {
-          const detail = await provider.getDetail(message.payload.hash)
-          panel.webview.postMessage({ command: 'see-detail', payload: detail })
-        }
-        if (message.command === 'hash-copied') {
-          vscode.window.setStatusBarMessage(`hash [${message.payload.hash}] was copied`, 3000)
-        }
-        if (['go-prev', 'go-next'].includes(message.command)) {
-          provider.setPageNum(message.payload.pageNum)
-          const html = await provider.getInitHtml()
-          panel.webview.html = html
-        }
-      },
-      undefined,
-      context.subscriptions
-    )
 
     try {
       provider.setConfig(config)
